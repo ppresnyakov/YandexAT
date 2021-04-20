1fa69e835e96da276b7f2884fa6a45f3c55030ca
ppresnyakov <ppresnyakov@yandex.ru>
2021-04-20 23:24:14 +0300

    Update

---
+++ yandex/src/main/java/yandex/market/util/AllureReporter.java
@@ -0,0 +1,27 @@
+package yandex.market.util;
+
+
+import gherkin.formatter.model.Result;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import ru.yandex.qatools.allure.Allure;
+import ru.yandex.qatools.allure.events.MakeAttachmentEvent;
+import static yandex.market.steps.Hooks.getDriver;
+
+
+public class AllureReporter extends ru.yandex.qatools.allure.cucumberjvm.AllureReporter {
+
+
+    @Override
+    public void result(Result result) {
+        if ("failed".equals(result.getStatus())) takeScreenshot(result);
+        super.result(result);
+    }
+
+    public void takeScreenshot(Result step) {
+        if (getDriver() != null) {
+            Allure.LIFECYCLE.fire(new MakeAttachmentEvent(((TakesScreenshot)getDriver()).getScreenshotAs(OutputType.BYTES),
+                    "Скриншот в момент ошибки", "image/png"));
+        }
+    }
+}
